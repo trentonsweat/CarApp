61843af61910f22b9d101e3d0a3c7beaf506f973
trentonsweat <trentonsweat@gmail.com>
2017-10-09 14:04:21 -0400

    CarApp

---
+++ CarApp/Car.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CarApp
+{
+    class Car
+    {   // Members 
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+       
+
+        // properties 
+        public string Make
+        {
+            set { make = value; }// changes the value of data 
+
+            get { return make; }// returns data
+
+        }
+
+        public string Model
+        {
+            set { model = value; }
+            get { return model; }
+
+        }
+
+        public int Year
+        {
+            set { year = value; }
+            get { return year; }
+
+        }
+
+        public double Price
+        {
+            set { price = value; }
+            get { return price; }
+
+        }
+
+
+
+        // methods 
+        public void AddCarDetails()
+        {
+            Console.WriteLine("Make");
+            Make = Console.ReadLine();
+
+            Console.WriteLine("Model");
+            Model = Console.ReadLine();
+
+            Console.WriteLine("Year");
+            Year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Price\n");
+            Price = double.Parse(Console.ReadLine());
+
+
+        }
+
+        // constructors 
+
+        public Car()// default constructor 
+        {
+            make = "";
+            model = "";
+            year = 0;
+            price = 0;
+        }
+
+
+        public Car(string CarMake, string CarModel, // constructor
+            int CarYear, double CarPrice)
+        {
+            make = CarMake;
+            model = CarModel;
+            year = CarYear;
+            price = CarPrice;
+
+        }
+    }
+}
---
+++ CarApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp
+{
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+
+                int NumberOfCars;
+                Console.WriteLine("Welcome to the Grand Circus Motors admin console! \nHow many cars are you entering:");
+                NumberOfCars = int.Parse(Console.ReadLine());
+
+                // 1. Create an array to hold the objeccts
+                Car[] CarInventory = new Car[NumberOfCars];
+
+            // 2. creating the Car objects inside our array (CarInventory)
+            for (int i = 0; i < CarInventory.Length; i++)
+               {
+                CarInventory[i] = new Car();// make an object 
+                }  
+
+                // 3. Initialize the objects 
+                for (int i = 0; i < CarInventory.Length; i++)
+                {
+                    Console.WriteLine("Enter Car #1 Make");
+                    CarInventory[i].Make = Console.ReadLine();
+
+                    Console.WriteLine("Enter Car Model");
+                    CarInventory[i].Model = Console.ReadLine();
+
+                    Console.WriteLine("Enter Car Year");
+                    CarInventory[i].Year = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter Car Price");
+                    CarInventory[i].Price = double.Parse(Console.ReadLine());
+                }
+
+
+
+                Console.WriteLine("\n...\nCurrent Inventory:");
+                Console.WriteLine("Make\tModel\tYear\tPrice");
+                foreach (Car CA in CarInventory)
+                {
+                    Console.WriteLine($"{CA.Make}\t{CA.Model}\t{CA.Year}\t{CA.Price}");
+
+                }
+
+
+
+
+            
+        }
+    }
+}
+    
+
